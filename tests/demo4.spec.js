670692da2818a01f3c5ed958fe366a646a1399fc
vigilanteX <dixitaman8@gmail.com>
2025-12-02 13:37:02 +0530

    getByText with exact match vs without exact match

--- tests/demo4.spec.js
+++ tests/demo4.spec.js
@@ -9,6 +9,14 @@ test('demo4 test', async ({page}) => {
      await page.getByRole('button',{name:'Get Sum',exact:true});
     //case2: get by role without exact match will select all matching elements matching the name partially
      await page.getByRole('button',{name:'Get Sum',exact:true});
+
+    
+     //getByText with exact match vs without exact match
+    //case1: get by text with exact match will select the exact matching element
+     const count=await page.getByText('Get Sum', {exact:true});
+    //case2: get by text without exact match will select all matching elements matching the text partially
+     const count2=await page.getByText('Get Sum', {exact:false});
+     console.log(await count.count(),await count2.count());
      
 
 })
