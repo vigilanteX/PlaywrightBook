c236202594cddfcf2038284e623f876c64c1b48b
vigilanteX <dixitaman8@gmail.com>
2025-12-02 12:43:00 +0530

    using css selector

--- tests/demo3.spec.js
+++ tests/demo3.spec.js
@@ -2,6 +2,13 @@ import {test, expect} from '@playwright/test';
 
 test('test3',async function({page})
 {
-    await page.goto('https://www.lambdatest.com/selenium-playground/simple-form-demo')
-    await page.locator('input#sum1').fill('10')
+    //using id selector
+    //await page.goto('https://www.lambdatest.com/selenium-playground/simple-form-demo')
+    // await page.locator('input#sum1').fill('10')
+    // await page.waitForTimeout(2000)
+
+    //using css selector
+    // await page.goto('https://www.lambdatest.com/selenium-playground/checkbox-demo')
+    // await page.locator("div.mt-40>label>input[type='checkbox']").click()
+    // await page.waitForTimeout(2000)
 })
