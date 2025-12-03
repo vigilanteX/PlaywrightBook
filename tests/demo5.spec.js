5e84789994223a4eb0c41aa1716e7295f57cafc7
vigilanteX <dixitaman8@gmail.com>
2025-12-03 12:36:03 +0530

    filter with hasText and hasNotText

--- tests/demo5.spec.js
+++ tests/demo5.spec.js
@@ -1,33 +1,39 @@
 import { expect, test } from "@playwright/test";
 
 test("demo6 test", async function ({ page }) {
-//   await page.goto("http://127.0.0.1:5500/index.html");
-//   page.on("dialog", async (dialog) => {
-//     await page.waitForTimeout(3000); // keep it open 3s
-//     await dialog.accept();
-//   });
+  //   await page.goto("http://127.0.0.1:5500/index.html");
+  //   page.on("dialog", async (dialog) => {
+  //     await page.waitForTimeout(3000); // keep it open 3s
+  //     await dialog.accept();
+  //   });
+  //example 1
+  //   await page.waitForTimeout(6000);
+  //   await page
+  //     .locator(".card")
+  //     .filter({ hasText: "Aman" })
+  //     .getByRole("button")
+  //     .click();
+  //   await page.waitForTimeout(6000);
+  //example 2
+  // await page.goto('http://127.0.0.1:5500/index2.html')
+  // await page.locator('div').filter({has:page.getByPlaceholder('First Name')}).getByRole('textbox').fill('Aman')
+  // await page.waitForTimeout(5000)
+  //example3
+  // await page.goto('http://127.0.0.1:5500/index2.html')
+  // await page.locator('div').filter({has:page.getByPlaceholder('First Name')}).locator('input').fill('Aman')
+  // await page.waitForTimeout(5000)
+  //example 4 hasText
+  //   await page.goto("http://127.0.0.1:5500/index2.html");
+  //   await page
+  //     .locator("div.parent")
+  //     .filter({ hasText: "Email" })
+  //     .locator("input")
+  //     .fill("nnbvcxza");
+  //   await page.waitForTimeout(5000);
 
-//example 1
-//   await page.waitForTimeout(6000);
-//   await page
-//     .locator(".card")
-//     .filter({ hasText: "Aman" })
-//     .getByRole("button")
-//     .click();
-//   await page.waitForTimeout(6000);
-
-
-//example 2
-// await page.goto('http://127.0.0.1:5500/index2.html')
-// await page.locator('div').filter({has:page.getByPlaceholder('First Name')}).getByRole('textbox').fill('Aman')
-// await page.waitForTimeout(5000)
-
-//example3
-await page.goto('http://127.0.0.1:5500/index2.html')
-await page.locator('div').filter({has:page.getByPlaceholder('First Name')}).locator('input').fill('Aman')
-await page.waitForTimeout(5000)
-
-
-
-
+  //example 5
+  await page.goto("http://127.0.0.1:5500/index2.html");
+  let ct = await page.locator("div.parent").filter({ hasNotText: "Email" });
+  console.log(await ct.count());
+  await page.waitForTimeout(20000);
 });
