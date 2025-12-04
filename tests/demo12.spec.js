4cbc15a909e9312990ad326ee2461516dd0cd327
vigilanteX <dixitaman8@gmail.com>
2025-12-04 13:35:44 +0530

    axios promise.all code

--- tests/demo12.spec.js
+++ tests/demo12.spec.js
@@ -2,8 +2,9 @@ import { expect, test } from "@playwright/test";
 import axios from "axios";
 
 test("demo11 test", async ({ page }) => {
-  //   const [download] = await Promise.all([
-  //     page.waitForEvent("download"), // Wait for the download event
-  //     page.click("#downloadBtn"), // Action that triggers the download
-  //   ]);
+  //await page.goto("https://example.com");
+  let [res1] = await Promise.all([
+    axios.get("http://localhost:3000/apiresponse1"),
+    axios.get("http://localhost:3000/apiresponse2"),
+  ]);
 });
