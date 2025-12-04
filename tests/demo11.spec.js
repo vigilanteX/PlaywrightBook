bbab534e8957b2d7e0673f7c395a02f35fd25444
vigilanteX <dixitaman8@gmail.com>
2025-12-04 12:51:02 +0530

    upload files

--- tests/demo11.spec.js
+++ tests/demo11.spec.js
@@ -1,9 +1,18 @@
 import { expect, test } from "@playwright/test";
 
 test("demo11 test", async ({ page }) => {
-  await page.goto(
-    "https://www.lambdatest.com/selenium-playground/upload-file-demo"
-  );
-  await page.setInputFiles("#file", "./buriburi.jpg");
+  //upload single file
+  // await page.goto(
+  //   "https://www.lambdatest.com/selenium-playground/upload-file-demo"
+  // );
+  // await page.setInputFiles("#file", "./buriburi.jpg");
+  // await page.waitForTimeout(8000);
+  //upload multiple files
+
+  await page.goto("https://blueimp.github.io/jQuery-File-Upload/");
+  await page.setInputFiles("input[multiple]", [
+    "./buriburi.jpg",
+    "./buriburi.jpg",
+  ]);
   await page.waitForTimeout(8000);
 });
