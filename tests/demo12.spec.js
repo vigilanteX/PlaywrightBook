79d53925060707eadcf766e3922157539ecf291a
vigilanteX <dixitaman8@gmail.com>
2025-12-04 13:32:40 +0530

    adding axios and checking how promise.all works

---
+++ tests/demo12.spec.js
@@ -0,0 +1,9 @@
+import { expect, test } from "@playwright/test";
+import axios from "axios";
+
+test("demo11 test", async ({ page }) => {
+  //   const [download] = await Promise.all([
+  //     page.waitForEvent("download"), // Wait for the download event
+  //     page.click("#downloadBtn"), // Action that triggers the download
+  //   ]);
+});
