0d0e6f1f764d90339b0853f80ad2756b9c2af6ee
vigilanteX <dixitaman8@gmail.com>
2025-12-08 19:49:35 +0530

    trace: "on-first-retry"

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  retries: 1,
+  retries: 3,
 
   testDir: "./tests",
   /* Run tests in files in parallel */
