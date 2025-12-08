3968c0d96db8a55c32807afef5829f1f1d6e68a1
vigilanteX <dixitaman8@gmail.com>
2025-12-08 19:53:13 +0530

    trace: "on-all-retries"

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on-all-retries",
     headless: false,
     testIdAttribute: "id",
     timeout: 60000,
