6f2bf5a8a682b46e61cdfeb1e3ff780f7a61ec1a
vigilanteX <dixitaman8@gmail.com>
2025-12-08 19:58:59 +0530

    trace: "on"

--- playwright.config.js
+++ playwright.config.js
@@ -13,8 +13,6 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  retries: 3,
-
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,7 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "off",
+    trace: "on",
     headless: false,
     testIdAttribute: "id",
     timeout: 60000,
