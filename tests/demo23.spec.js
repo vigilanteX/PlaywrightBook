17e955b9435df958344cc8de9a0b3f99bd995253
vigilanteX <dixitaman8@gmail.com>
2025-12-09 22:34:37 +0530

    using-npx playwright test demo23.spec.js --grep "@smoke" to run by tag

---
+++ tests/demo23.spec.js
@@ -0,0 +1,29 @@
+import { test } from "@playwright/test";
+
+test("demo22 test", async ({ page }) => {
+  console.log("demo22 test");
+});
+
+test("demo221 test", async ({ page }) => {
+  console.log("demo221 test");
+});
+
+test("demo223 test", async ({ page }) => {
+  console.log("demo223 test");
+});
+
+test.skip("demo224 @smoke test", async ({ page }) => {
+  console.log("demo224 test");
+});
+test.fail("demo225 @smoke test", async ({ page }) => {
+  throw new Error("Intentional failure");
+});
+test.fixme("demo226 test", async ({ page }) => {
+  console.log("demo226 test");
+});
+
+test("demo227 test", async ({ page }) => {
+  test.slow();
+  await page.goto("https://yahoo.com");
+  console.log("demo227 test");
+});
