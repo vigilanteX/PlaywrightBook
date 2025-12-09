d76048e5eb87075e7411612e9c455e99facde49e
vigilanteX <dixitaman8@gmail.com>
2025-12-09 22:04:35 +0530

    skip testcase

---
+++ tests/demo22.spec.js
@@ -0,0 +1,17 @@
+import { test } from "@playwright/test";
+
+test("demo22 test", async ({ page }) => {
+  console.log("demo22 test");
+});
+
+test("demo221 test", async ({ page }) => {
+  console.log("demo221 test");
+});
+
+test("demo223 test", async ({ page }) => {
+  console.log("demo223 test");
+});
+
+test.skip("demo224 test", async ({ page }) => {
+  console.log("demo224 test");
+});
