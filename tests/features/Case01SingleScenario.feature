9a46f07b91402cd09c4e5cdbc7a33cf42315021b
vigilanteX <dixitaman8@gmail.com>
2025-12-10 18:57:27 +0530

    Starting gherkins and cucumber aagain

--- tests/features/Case01SingleScenario.feature
+++ tests/features/Case01SingleScenario.feature
@@ -1,6 +1,6 @@
 Feature: Test login and logout
 
   Scenario: Customer try success login and logout
-    Given User opens naveen automation labs and provide credentials
+    Given User opens naveen's website and add credentials
     When User clicks on login
     Then User should land on website as logged in user
---
+++ tests/step_definations/Case01SingleScenario.js
@@ -0,0 +1,14 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+
+Given("User opens naveen's website and add credentials", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("came to naveen automation");
+});
+When("User clicks on login", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("user enters details");
+});
+Then("User should land on website as logged in user", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("user logs in");
+});
