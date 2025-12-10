af74929d825ed58ee646375b0c3f92e5b2efbec1
vigilanteX <dixitaman8@gmail.com>
2025-12-10 19:09:24 +0530

    using backgrround in feature to run common code

---
+++ tests/features/Case02UsingBackground.feature
@@ -0,0 +1,12 @@
+Feature: Checking backgroung in feature file
+
+  Background:
+    Given open url
+
+  Scenario: Scenario 1 we are testing
+    When scenario1 occurs happen click
+    Then store scenario1 result
+
+  Scenario: Scenario 2 we are testing
+    When scenario2 occurs happen click
+    Then store scenario2 result
---
+++ tests/step_definations/Case02UsingBackground.js
@@ -0,0 +1,22 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+
+Given("open url", function () {
+  console.log("common url");
+});
+
+When("scenario1 occurs happen click", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("scenario1 occurs happen click");
+});
+Then("store scenario1 result", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("store scenario1 result");
+});
+When("scenario2 occurs happen click", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("scenario2 occurs happen click");
+});
+Then("store scenario2 result", function () {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("store scenario2 result");
+});
