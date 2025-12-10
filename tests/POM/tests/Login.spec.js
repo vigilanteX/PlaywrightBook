7b4a42e59c8f998bb41d23da01182bfa89d4e7ec
vigilanteX <dixitaman8@gmail.com>
2025-12-10 11:38:04 +0530

    using external json fiel for data

--- tests/POM/tests/Login.spec.js
+++ tests/POM/tests/Login.spec.js
@@ -1,9 +1,11 @@
 import { test, browser } from "@playwright/test";
 import LoginPage from "../pages/LoginPage.js";
 import MyAccount from "../pages/MyAccount.js";
+import sitData from "../data/sit.json";
 
 test.describe("Login Tests", () => {
   test("Valid Login Test1", async ({ browser }) => {
+    let data = sitData.data;
     let context = await browser.newContext();
     let page = await context.newPage();
     await page.goto(
@@ -11,7 +13,7 @@ test.describe("Login Tests", () => {
     );
     const loginPage = new LoginPage(page, context);
     const myaccount = new MyAccount(page, context);
-    await loginPage.login("bobatea@gmail.com", "12345");
+    await loginPage.login(data[0].email, data[0].password);
     console.log(await myaccount.myAccountGetUrl());
     await myaccount.logOut();
   });
