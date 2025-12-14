d44c4648b67f979767621bc5914b05c20fddc77f
vigilanteX <dixitaman8@gmail.com>
2025-12-14 17:25:08 +0530

    Revisit-page.on vs waitForEvent

--- playwright.config.js
+++ playwright.config.js
@@ -13,9 +13,9 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  globalSetup: "./global-setup.js",
+  // globalSetup: "./global-setup.js",
   reporter: [["junit", { outputFile: "meow.xml" }]],
-  testDir: "./tests",
+  testDir: "./TRYREVISION",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    storageState: "./LoginAuth.json",
+    // storageState: "./LoginAuth.json",
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
