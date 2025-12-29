9c85055cd4c9059e0879ee254e48e21dc7cd5cb2
vigilanteX <dixitaman8@gmail.com>
2025-12-29 10:21:58 +0530

    locator.dblclick()

--- RevisitDec29/LocatorBasedAction.spec.js
+++ RevisitDec29/LocatorBasedAction.spec.js
@@ -1,8 +1,14 @@
 import { test } from "@playwright/test";
 
-test("autowait locators locator.click()", async function ({ page }) {
+test.skip("autowait locators locator.click()", async function ({ page }) {
   await page.goto(
     "https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo"
   );
   await page.click("//p[contains(text(),'Alert')]/button");
+});
+
+test("locator.dblclick()", async function ({ page }) {
+  await page.goto("https://doubleclicktest.com/");
+  await page.dblclick("//b[text()='Click Here to Test']");
+  await page.waitForTimeout(4000);
 });
