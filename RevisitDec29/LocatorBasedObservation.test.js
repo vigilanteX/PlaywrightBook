5181963e9eec18743111eb702fb03c95197fc5e6
vigilanteX <dixitaman8@gmail.com>
2025-12-30 15:44:48 +0530

    locator.isChecked()

--- RevisitDec29/LocatorBasedObservation.test.js
+++ RevisitDec29/LocatorBasedObservation.test.js
@@ -16,3 +16,20 @@ test("locator.isHidden()", async function ({ page }) {
   console.log(await page.locator("//label[text()='meow']").isHidden());
   await page.waitForTimeout(4000);
 });
+
+test("locator.isChecked()", async function ({ page }) {
+  await page.goto(
+    "https://www.lambdatest.com/selenium-playground/checkbox-demo"
+  );
+  await page.locator("//label[text()='Click on check box']/input").check();
+  await page.waitForTimeout(4000);
+  console.log(
+    await page.locator("//label[text()='Click on check box']/input").isChecked()
+  );
+  await page.waitForTimeout(4000);
+  await page.locator("//label[text()='Click on check box']/input").uncheck();
+  console.log(
+    await page.locator("//label[text()='Click on check box']/input").isChecked()
+  );
+  await page.waitForTimeout(4000);
+});
