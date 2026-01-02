e871a167b6b2620d3a6ec646190f4122bba1a8e5
vigilanteX <dixitaman8@gmail.com>
2026-01-02 11:46:13 +0530

    mock response using route.fullfill()

--- RevisitDec29/mocking/demo1.spec.js
+++ RevisitDec29/mocking/demo1.spec.js
@@ -1,15 +1,29 @@
 import { test } from "@playwright/test";
 
-test("mock request", async function ({ page }) {
+test.skip("mock request", async function ({ page }) {
   await page.goto("http://127.0.0.1:5500/RevisitDec29/mocking/index.html");
+  //listener page.route()
   await page.route("**/playwrightserver", async function (r) {
     const req = r.request();
     console.log(await req.allHeaders());
     let body = JSON.parse(await req.postData());
     body = { userid: 123456789, ...body };
     console.log(body);
+    //send request
     await r.continue({ postData: body });
   });
 
   await page.locator("#callApiBtn").click();
+});
+
+test("mock response", async function ({ page }) {
+  await page.goto("http://127.0.0.1:5500/RevisitDec29/mocking/index.html");
+  await page.waitForTimeout(5000);
+
+  await page.route("**/playwrightserver", async function (r) {
+    const data = { name: "Aman", age: 100, meow: "CAT" };
+    await r.fulfill({ body: JSON.stringify(data) });
+  });
+  await page.locator("#callApiBtn").click();
+  await page.waitForTimeout(5000);
 });
